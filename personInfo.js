ebed91286912745ad338f4b4902cfff6714b8776
Desislava Kracheva <desislava.92@abv.bg>
2021-11-12 23:45:10 +0200

    new ex.

---
+++ personInfo.js
@@ -0,0 +1,20 @@
+function solve(firstName, lastName, age) {
+
+    let person = {
+
+        firstName,
+        lastName,
+        age
+
+
+    }
+    return person;
+
+
+}
+
+
+console.log(solve("Peter", 
+"Pan",
+"20"
+))
